bbd24c46e6da8ee4d2fdd49eedbce5bee56e0ea3
Vinicius Reis <viniciusreissantana@gmail.com>
2018-05-13 00:00:43 -0300

    Commit iniciando projeto.

---
+++ src/main/java/com/teste/corridas/SistemaCorridasApplication.java
@@ -0,0 +1,12 @@
+package com.teste.corridas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCorridasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCorridasApplication.class, args);
+	}
+}
